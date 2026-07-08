25d59b01463a19f860e1270af248cfd7aa9943c1
Soraya Cazorla Guillén <sorayacazorlag@gmail.com>
2026-07-08 11:18:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // 1. CAMBIAR POR URL Y KEY DE TU API
-const SUPABASE_URL = 'https://ifgzperqnxoomyvvytzr.supabase.co/rest/v1/forms';
-const SUPABASE_API_KEY = 'sb_publishable_s4C8y1rgb321cEF5B969MA_3jMwVpNJ';
+const SUPABASE_URL = 'https://vzivzwonwuowsdfdbpma.supabase.co';
+const SUPABASE_API_KEY = 'sb_publishable_5AviGsm_n2ZX9fUEUcJQBQ_WlGOtOpX';
 
 
 //ENVIAR DATOS DE UN FORMULARIO A UNA API CON POST
