e79bac6933e08d20950997d957b03c3ef49b383b
Soraya Cazorla Guillén <sorayacazorlag@gmail.com>
2026-07-09 10:41:50 +0200

    Update create.sql

---
+++ sql/create.sql
@@ -0,0 +1,14 @@
+INSERT INTO form2 (name, email, subject, age, message)
+VALUES 
+('Ana', 'ana@gmail.com', 'information', NULL, 'hola testing'),
+(NULL, 'pedro@gmail.com', 'job', 20, 'Pedro mensaje' ),
+('María', 'maria@yahoo.com', 'other', 25, 'mensaje de prueba'),
+('Ana', 'ana.work@gmail.com', 'job', 30, 'test message'),
+(NULL, 'user1@gmail.com', 'information', 20, 'information mensaje testing'),
+('Rubén', 'rubenn@gmail.com', 'other', NULL, 'web test'),
+('Leire', 'leire_job@hotmail.com', 'job', 20, 'adjunto cv'),
+('Ramón', 'ramon-hernandez@yahoo.es', 'information', NULL, 'testing'),
+('Juan', 'juan_fernandez-2@gmail.com', 'other', 19, 'testeo1'),
+('Sofía', 'soophie@hotmail.com', 'job', NULL, 'pruebas API'),
+('Clara', 'clara_234@yahoo.es', 'information', 22, 'text message'),
+('Joao', 'joao-32@hotmail.com', 'other', 27, 'question');
