9f28362ee536c4259dab666aca267a5c996f042e
Soraya Cazorla Guillén <sorayacazorlag@gmail.com>
2026-07-09 10:42:33 +0200

    Update update.sql

---
+++ sql/update.sql
@@ -0,0 +1,9 @@
+--Update María's email to 'mariatest@gmail.com'
+UPDATE form2
+SET email = 'mariatest@gmail.com'
+WHERE name = 'María';
+
+--Update ages from 20 to 21
+UPDATE form2
+SET age = 21
+WHERE age = 20;
